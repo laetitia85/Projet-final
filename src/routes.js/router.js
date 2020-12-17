cd3e0c2195ba34cbc44a553b7244cd0928c71daf
Laetitia de Bardin <titiluna.sky85@gmail.com>
2020-12-17 12:23:11 +0100

    postUsersSignUp

--- src/routes.js/router.js
+++ src/routes.js/router.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express.Router();
 const sql = require("../database/database.js");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 10;
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 // const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
@@ -13,6 +13,19 @@ app.get("/users", (req, res) => {
     }
     res.send(response);
   });
+});
+
+app.post("/users/sign-up", (req, res) => {
+  try {
+    bcrypt.hash(req.body.password, saltRounds).then(function(passwordHash) {
+      sql.query(
+        `INSERT INTO artists_users (name,first_name,email,password,picture_profil) VALUES ('${req.body.name}','${req.body.first_name}','${req.body.email}','${passwordHash}','${req.body.picture_profil}')`
+      );
+    });
+    res.status(200).send("Vous etes bien enregistrer");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = app;
