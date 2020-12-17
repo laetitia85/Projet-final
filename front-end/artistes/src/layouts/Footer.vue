95b352bd254db8c770c7d0972068755bf8255880
Laetitia de Bardin <titiluna.sky85@gmail.com>
2020-12-17 12:47:25 +0100

    layouts

---
+++ front-end/artistes/src/layouts/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- Footer -->
+  <mdb-footer color="blue" class="font-small pt-4 mt-4">
+    <mdb-container class="text-left">
+      <mdb-row>
+        <mdb-col sm="6">
+          <p>2020 Copyright</p>
+          <p>Laetitia de Bardin</p>
+        </mdb-col>
+        <mdb-col sm="6">
+          <ul>
+            <li class="list-unstyled"><a href="#!">Mentions Légales</a></li>
+            <li class="list-unstyled"><a href="#!">CGU</a></li>
+          </ul>
+        </mdb-col>
+        <mdb-col sm="6">
+          <ul>
+            <li class="list-unstyled"><a href="#!">contact</a></li>
+          </ul>
+          <ul class="list-unstyled list-inline">
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm btn-fb mx-1"
+                ><i class="fab fa-facebook"> </i
+              ></a>
+            </li>
+            <li class="list-inline-item">
+              <a class="btn-floating btn-sm btn-tw mx-1"
+                ><i class="fab fa-twitter"> </i
+              ></a>
+            </li>
+          </ul>
+        </mdb-col>
+      </mdb-row>
+    </mdb-container>
+  </mdb-footer>
+  <!-- Footer -->
+</template>
+
+<script>
+import { mdbFooter, mdbContainer, mdbRow, mdbCol } from "mdbvue";
+export default {
+  name: "FooterPage",
+  components: {
+    mdbFooter,
+    mdbContainer,
+    mdbRow,
+    mdbCol,
+  },
+};
+</script>
---
+++ front-end/artistes/src/layouts/Navbar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="conteneur">
+    <mdb-navbar position="top" dark color="indigo" scrolling>
+      <mdb-navbar-toggler>
+        <mdb-navbar-nav>
+          <mdb-nav-item href="<Profil/>" active>Profil</mdb-nav-item>
+          <mdb-nav-item href="#">Créations</mdb-nav-item>
+          <mdb-nav-item href="#">Mes posts</mdb-nav-item>
+        </mdb-navbar-nav>
+        <p>
+          <b-button variant="outline-primary" @click="DeleteToken()"
+            >Sign-Out</b-button
+          >
+        </p>
+      </mdb-navbar-toggler>
+    </mdb-navbar>
+  </div>
+</template>
+
+<script>
+import { mdbNavbar, mdbNavItem, mdbNavbarNav, mdbNavbarToggler } from "mdbvue";
+export default {
+  name: "Navbar",
+  components: {
+    mdbNavbar,
+    mdbNavItem,
+    mdbNavbarNav,
+    mdbNavbarToggler,
+  },
+
+  methods: {
+    DeleteToken() {
+      this.$store.dispatch("deleteToken").then(() => {
+        this.$router.push("/");
+      });
+    },
+  },
+};
+</script>
+
+<style lang="postcss" scoped>
+#conteneur {
+  display: flex;
+  justify-content: space-between;
+  border: 3px solid black;
+  width: 100%;
+  margin: 0px 0px 0px 0px;
+  background-color: black;
+  box-shadow: 6px 6px 0px blue;
+  color: blue;
+}
+p {
+  margin: auto;
+}
+</style>
